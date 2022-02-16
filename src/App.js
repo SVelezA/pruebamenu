b0d16be54a183fc56d7c982414537c10d832cc7b
SVelezA <velezaristizabal@gmail.com>
2022-02-16 18:22:05 -0500

    2do Commit

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './home';
-import conozcanos from './pages/conozcanos';
-import clasificados from './pages/clasificados';
+import Home from './pages/home';
+import Conozcanos from './pages/Conozcanos';
+import Clasificados from './pages/clasificados';
 
 function App() {
   return (
@@ -12,9 +12,9 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route exact path='/home' exact component={Home} />
-          <Route exact path='/conozcanos' component={conozcanos} />
-          <Route exact path='/clasificados' component={clasificados} />
+          <Route exact path='/home' exact element={<Home />} />
+          <Route exact path='/conozcanos' element={<Conozcanos />} />
+          <Route exact path='/clasificados' element={<Clasificados />} />
         </Routes>
       </Router>
     </>
